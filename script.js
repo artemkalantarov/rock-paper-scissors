0d68123e7c89413908155e55b3ac9f0e0cd2d354
Art <artem1324@gmail.com>
2023-09-13 17:37:16 -0700

    Finalize and clean up code
    Removed unnecessary code and renamed js file from index.js to
    script.js.

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 // Function generates computer's choice of rock, paper or scissors
 function getComputerChoice() {
 
-    //Generates a random number from 0-2
     let randNumber = Math.floor(Math.random() * 3)
     
-    //Each number is matched with rock, paper or scissors
     if (randNumber === 0) {
         return "Rock";
     } else if (randNumber === 1) {
@@ -14,17 +12,10 @@ function getComputerChoice() {
     }
 }
 
-// Function prompts user for user's choice and returns it
-function getUserSelection(playerSelection) {
-    //Capitalizes the answer
-    return (playerSelection.charAt(0)).toUpperCase() + (playerSelection.slice(1)).toLowerCase();
-}
-
 // Function checks who won the round by getting user's and computer's choice
-// User's or computer's score increases depending on who won
-// Returns an explanation of who won and what beat what
 function playRound(playerSelection) {
-    playerSelection = getUserSelection(playerSelection);
+    playerSelection = (playerSelection.charAt(0)).toUpperCase() +
+                        (playerSelection.slice(1)).toLowerCase();
     const computerSelection = getComputerChoice();
     if (playerSelection === computerSelection) {
         resultText.textContent = `It is a tie! Both players had ${playerSelection}!`
@@ -51,7 +42,6 @@ function playRound(playerSelection) {
   }
 
 // Function checks score at the end of the game and declares a winner
-// Function also states the final score
 function checkScore() {
     if (computerScore > playerScore) {
         return `Game over! Computer won!`
@@ -63,7 +53,6 @@ function checkScore() {
 }
 
 // Function starts the game and runs it for 5 rounds
-// After all rounds are played, declares the winner
 function gameOver(playerScore, computerScore) {
     if (playerScore === 5 || computerScore === 5) {
         resultText.textContent = checkScore();
@@ -73,6 +62,7 @@ function gameOver(playerScore, computerScore) {
     
 }
 
+//Function to play the game again
 function playAgain(){
     computerScore = 0;
     computerScoreDisplay.textContent = 0;
@@ -83,10 +73,8 @@ function playAgain(){
     replayButton.classList.add('hidden');
 }
 
-// Numeric variable for computer's score
-let computerScore = 0
 
-// Numeric variable for player's score
+let computerScore = 0
 let playerScore = 0
 const buttonsContainer = document.querySelector('.buttons');
 const replayButton = document.querySelector('.replay-button');
@@ -94,6 +82,7 @@ const resultText = document.querySelector('.result-text');
 const playerScoreDisplay = document.querySelector('.player-score');
 const computerScoreDisplay = document.querySelector('.computer-score');
 const buttons = document.querySelectorAll('.play-button');
+
 buttons.forEach(button => button.addEventListener('click', () => playRound(button.id)));
 replayButton.addEventListener('click', () => playAgain());
 
